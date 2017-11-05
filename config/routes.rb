05c283ee9b0f54d403be96464eb2234bf2daa9a7
puka89 <ludiat.lud@abv.bg>
2017-11-05 23:51:39 +0300

    messages

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :notes, param: :slug
   root 'notes#index' 
   get '/notes/:slug/info', to: 'notes#info'
-  post '/notes/api', to: 'notes#create'
+  post '/messages/api', to: 'notes#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
